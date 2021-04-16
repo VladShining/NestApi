41c56d5b5e549fed9ef99ff2da611d7e006e0d93
nca49 <vladshining@gmail.com>
2021-04-16 12:55:28 +0300

    env heroku

--- src/app/app.controller.spec.ts
+++ src/app/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "Welcome to nest api 49"', () => {
+      expect(appController.getHello()).toBe('Welcome to nest api 49');
     });
   });
 });
